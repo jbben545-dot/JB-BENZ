c7ff60cedd25c1d394b0ad049f2dbc013b6daa9e
Jerome <jeromebenjemin@gmail.com>
2026-01-15 15:48:44 +0630

    I am doing a branch training

---
+++ untitled/src/main/main.java
@@ -0,0 +1,8 @@
+public class Main{
+    static void main() {
+        IO.pritnln(String.formate("Hellow and Welcome!"))
+                for(int i=1;i<=5 ;i++){
+              IO.println("")
+    }
+    }
+}
